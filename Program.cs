bfdd109b44c8c2c4758e5030f9f8fa14416d78ea
Saibot155 <92667455+Saibot155@users.noreply.github.com>
2023-09-06 15:50:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloEdugrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"Hej,{name}, på {currentDate}");
+        }
+    }
+}
